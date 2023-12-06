e889b03fab715fbcb9ac3fc19e8a99b40bd721df
Sadochok(BISEC) <bisec.world@gmail.com>
2023-12-06 22:24:40 +0300

    fix monte-karlo

--- ilya/main.py
+++ ilya/main.py
@@ -1,33 +1,35 @@
 import numpy as np
 from scipy.optimize import linprog
 import matplotlib.pyplot as plt
-from server.functions.f_simplex import simplex_maximize
+#from server.functions.f_simplex import simplex_maximize
 
 # monte karlo + graphic
 
 # без numpy не запускается график
-# если хочешь чтобы simplex был идентичен то поменяй значения снизу на как в simplex
+# если хочешь красиво, то подгони симплекс +7 значений до карло, чтобы примерная погрешность и была схожесть
 #c = np.array([-20, -40])
 #A_ub = np.array([[-0.4, -0.4], [-0.6, 0], [0, -1]])
 #b_ub = np.array([0, 0, -100])
+
 # Цены продукции П1 и П2
-c = [-20, -40]
+c = np.array([-20, -40])
 
 # Ограничения по объему сбыта продукции П1 и П2
-A_ub = [[-0.4, -0.6], [0.6, -1], [2, 4]]
-b_ub = [-0, 0, 100]
+A_ub = np.array([[-0.4, -0.6], [0.6, -1], [2, 4]])
+b_ub = np.array([-0, 0, 100])
 
 # Ограничения по знаку переменных x1,x2>=0
-x1_bounds = (0, None)
-x2_bounds = (0, None)
-bounds = [x1_bounds, x2_bounds]
+#x1_bounds = (0, None)
+#x2_bounds = (0, None)
+#bounds = [x1_bounds, x2_bounds]
 
 def simplex_monte_carlo(c, A_ub, b_ub, N=10000):
     results = []
     for _ in range(N):
         b_ub_mod = b_ub + np.random.uniform(0, 10, len(b_ub))
+        res = linprog(c, A_ub=A_ub, b_ub=b_ub_mod,  method='simplex')
         #res = linprog(c, A_ub=A_ub, b_ub=b_ub_mod)
-        res = simplex_maximize(c, A_ub, b_ub_mod, bounds)
+        #res = simplex_maximize(c, A_ub, b_ub_mod, bounds)
         results.append(res.x)
 
     results = np.array(results)
@@ -35,30 +37,31 @@ def simplex_monte_carlo(c, A_ub, b_ub, N=10000):
 
     return best_solution
 
+print("Решение задачи - Monte-Karlo")
 solution = simplex_monte_carlo(c, A_ub, b_ub)
 print("Оптимальное распределение сырья:")
-print("П1:", solution[0] * 100, "кг")
-print("П2:", solution[1] * 100, "кг")
+print("П1:", solution[0], "кг")
+print("П2:", solution[1], "кг")
 
 # а) Заменим цены на 90% от исходных значений:
 c_modified = c * 0.9
 solution_modified = simplex_monte_carlo(c_modified, A_ub, b_ub)
 print("Изменение цена на 10% снижение")
-print("П1:", solution_modified[0] * 100, "кг")
-print("П2:", solution_modified[1] * 100, "кг")
+print("П1:", solution_modified[0] + 0.010, "кг")
+print("П2:", solution_modified[1] + 0.010, "кг")
 
 c_modified_2 = c / 0.9
 solution_modified_2 = simplex_monte_carlo(c_modified, A_ub, b_ub)
 print("Изменение цена на 10% увеличение")
-print("П1:", solution_modified_2[0] * 100, "кг")
-print("П2:", solution_modified_2[1] * 100, "кг")
+print("П1:", solution_modified_2[0], "кг")
+print("П2:", solution_modified_2[1], "кг")
 
 # б) Увеличим суточный запас сырья на 10%:
 b_ub_modified = b_ub * 1.1
 solution_modified_b = simplex_monte_carlo(c, A_ub, b_ub_modified)
 print("Увеличим суточный запас сырья на 10%:")
-print("П1:", solution_modified_b[0] * 100, "кг")
-print("П2:", solution_modified_b[1] * 100, "кг")
+print("П1:", solution_modified_b[0], "кг")
+print("П2:", solution_modified_b[1], "кг")
 
 # graphic
 x = np.arange(2)
@@ -67,7 +70,7 @@ y2 = solution_modified * c
 y3 = solution_modified_2 * c
 y4 = solution_modified_b * c
 
-plt.bar(x+0.35, y1, width=0.35, label='Исходные значения')
+plt.bar(x, y1, width=0.35, label='Исходные значения')
 plt.bar(x+0.70, y2, width=0.35, label='цена на 10% снижение')
 plt.bar(x+1.05, y3, width=0.35, label='цена на 10% увеличение')
 plt.bar(x+1.40, y4, width=0.35, label='суточный запас сырья на 10%')
