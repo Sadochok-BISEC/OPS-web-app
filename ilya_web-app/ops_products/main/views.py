413f721c14e9e17bc66d1ec75f879d335ed37d6b
Sadochok(BISEC) <bisec.world@gmail.com>
2023-12-11 03:53:58 +0300

    contacs datum added

--- ilya_web-app/ops_products/main/views.py
+++ ilya_web-app/ops_products/main/views.py
@@ -1,8 +1,35 @@
 from django.shortcuts import render
 # Create your views here.
 
-def show_test(request):
-    return render(request, 'main/main.html')
+data = {
+    # In the form(on the right)
+    'obj_titles':{
+        'main_page': 'Привет!',
+        'about': 'О проекте',
+        'contacts': 'Контакты',
+    },
+    'obj_contacts_info':{
+        'txt_message':'Наше Веб-приложение, разработано по решению оптимизационных задач', # About.html call
+        'author_name':'Автор: Ilya Bisec',
+        'email':'Почта: ilya.borisov.bisec@gmail.com',
+        'phone_number':'Телефон:(29)4911431 - Беларусь',
+    },
+    'obj_tab_titles':{
+        'main_page':'Главная страница',
+        'simplex_page':'Симплекс метод',
+        'monte_page':'Монте-Карло метод',
+        'about_page':'О проекте',
+        'contacts_page':'Контакты',
+        'exit_page':'Выйти',
+    }
+}
+
+
+def home(request):
+    return render(request, 'main/main.html', data)
 
 def about(request):
-    return render(request, 'main/about.html')
+    return render(request, 'main/about.html', data)
+
+def contacts(request):
+    return render(request, 'main/contacts.html', data)
