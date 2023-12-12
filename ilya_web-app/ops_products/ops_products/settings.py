5b425d1cf484281eeb0ef9fd8a9e677907ecc26a
Sadochok(BISEC) <bisec.world@gmail.com>
2023-12-12 04:46:19 +0300

    add redirect

--- ilya_web-app/ops_products/ops_products/settings.py
+++ ilya_web-app/ops_products/ops_products/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'main',
+    'users',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -127,3 +128,6 @@ STATICFILES_DIRES = [
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = ''
