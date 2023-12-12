b6cefe4f17222a5948fddad9f7da1ede94130820
Sadochok(BISEC) <bisec.world@gmail.com>
2023-12-12 04:44:53 +0300

    users page is the main page on the default

--- ilya_web-app/ops_products/ops_products/urls.py
+++ ilya_web-app/ops_products/ops_products/urls.py
@@ -20,6 +20,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('main.urls'))
+    path('ops_products/', include('main.urls')),
+    #path('contacts/', include('django.contrib.auth.urls')),
+    path('', include('users.urls')),
 
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
