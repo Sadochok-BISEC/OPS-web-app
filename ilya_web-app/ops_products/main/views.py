71c0d92d51bebb7a8fc0b47a6037d0c1bc83b4d5
Sadochok(BISEC) <bisec.world@gmail.com>
2023-12-18 06:41:04 +0300

    moved data to file

--- ilya_web-app/ops_products/main/views.py
+++ ilya_web-app/ops_products/main/views.py
@@ -3,36 +3,11 @@ from django.contrib.auth.decorators import login_required
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 from .forms import RegisterForm
+
+from txt_data_file import data
 # Create your views here.
 
-data = {
-    # In the form(on the right)
-    'obj_message_titles':{
-        'hello': 'Привет!',
-        'hello_question': 'Привет! Здесь впервые?',
-        'about': 'О проекте',
-        'contacts': 'Контакты',
-    },
-    'obj_contacts_info':{
-        'txt_message':'Наше Веб-приложение, разработано по решению оптимизационных задач', # About.html call
-        'author_name':'Автор: Ilya Bisec',
-        'email':'Почта: ilya.borisov.bisec@gmail.com',
-        'phone_number':'Телефон:(29)4911431 - Беларусь',
-    },
-    'obj_tab_titles':{
-        # Generally
-        'main_page':'Главная страница',
-        'simplex_page':'Симплекс метод',
-        'monte_page':'Монте-Карло метод',
-        'about_page':'О проекте',
-        'contacts_page':'Контакты',
-        'exit_page':'Выйти',
-        # Auth
-        'not_registered_page': 'Добро пожаловать!',
-        'login_page': 'Авторизация',
-        'signup_page': 'Регистрация',
-    }
-}
+
 @login_required
 def home(request):
     return render(request, 'main/main.html', data)
